8f23856e2cb6194bc913d2018aa10bd27e7d0a96
unknown <gstabby@163.com>
2020-10-01 03:29:56 +0800

    新增主页入场动画

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="home">
+    <div class="start_box">
+      <div class="start_title_box">
+        <h1 class="start_title start_title_on">Tuffy</h1>
+        <h1 class="start_title start_title_back">Tuffy</h1>
+      </div>
+    </div>
     <div class="home_box container-fluid" :style="currentTopic.bgColor">
       <div class="row row-vertical-center justify-content-center" style="height: 100vh;">
         <div class="col-xl-4 col-lg-6">
-          <h3 class="welcome_text on_text">WELCOME</h3>
-          <h3 class="welcome_text shadow_text">WELCOME</h3>
+          <h3 class="welcome_text">WELCOME</h3>
           <h2 class="tuffy_text">TUFFY's LAIR</h2>
           <p class="tuffy_intro f-17">嗨，你好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的小玩意，祝您在此探索愉快,嗨，你好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的小玩意，祝您在此探索愉快,嗨，你好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的小玩意，祝您在此探索愉快</p>
           <button class="enter_button f-17" :style="currentTopic.buttonColor" @click="toLink(currentTopic.link)">{{ currentTopic.buttonText }}</button>
@@ -77,12 +82,80 @@ export default {
 
 <style scoped>
 button:focus {
-  outline:0;
+  outline: 0;
+}
+.start_box {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  z-index: 1000;
+  background: #39323A;
+  color: white;
+  display: flex;
+  justify-content: center;
+  animation: circle-out ease-in-out 2s 1.3s forwards;
+}
+.start_title_box {
+  align-self: center;
+  justify-self: center;
+  width: 220px;
+  height: 110px;
+  animation: up-in .8s linear;
+}
+.start_title {
+  font-weight: bolder;
+  font-size: 90px;
+  user-select: none;
+  -webkit-user-select: none;
+  position: absolute;
+}
+.start_title_on {
+  color: transparent;
+  -webkit-text-stroke: #4E474F 1px;
+}
+.start_title_back {
+  clip-path: circle(0% at 0% 0%);
+  animation: circle-in 1s 1s linear forwards;
+}
+@keyframes up-in {
+  0% {
+    opacity: 0;
+    transform: translateY(40px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+@keyframes circle-in {
+  0% {
+    clip-path: circle(0% at 0% 0%);
+  }
+  /* 50% {
+    clip-path: circle(150% at 0% 0%);
+  } */
+  100% {
+    clip-path: circle(150% at 0% 0%);
+  }
+}
+@keyframes circle-out {
+  0% {
+    clip-path: circle(100% at 50% 50%);
+  }
+  /* 80% {
+    clip-path: circle(3% at 50% 50%);
+  }
+  90% {
+    clip-path: circle(10% at 50% 50%);
+  } */
+  100% {
+    clip-path: circle(0% at 50% 50%);
+  }
 }
 .home_box {
   height: 100vh;
   color: white;
-  font-family: Helvetica;
+  /* font-family: Helvetica; */
   transition: background 1s ease;
 }
 .row-vertical-center {
@@ -92,18 +165,26 @@ button:focus {
 .welcome_text {
   font-size: 150px;
   line-height: 100px;
-}
-.on_text {
   color: transparent;
-  -webkit-text-stroke: white 1px;
-}
-.shadow_text {
-  position: absolute;
-  top: 14px;
-  animation: bowen 5s ease infinite;
+  -webkit-text-stroke: #4E474F 1px;
+  animation: color-in 2s 3s ease forwards;
 }
 .tuffy_text {
-  font-size: 80px;
+  font-size: 90px;
+  font-weight: bolder;
+  color: transparent;
+  -webkit-text-stroke: #4E474F 1px;
+  animation: color-in 2s 4s ease forwards;
+}
+@keyframes color-in {
+  0% {
+    color: transparent;
+    /* clip-path: circle(200% at 50% 50%); */
+  }
+  100% {
+    color: white;
+    /* clip-path: circle(0% at 50% 50%); */
+  }
 }
 .tuffy_intro {
   width: 800px;
