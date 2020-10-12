941cacb4b73a3936981bf5f81d4c9afcd645e910
unknown <gstabby@163.com>
2020-10-12 19:10:38 +0800

    重构主页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,14 @@
     </div>
     <div class="home_box container-fluid" :style="currentTopic.bgColor">
       <div class="row row-vertical-center justify-content-center" style="height: 100vh;">
-        <div class="col-xl-4 col-lg-6">
+        <div class="col-xl-4 col-lg-4 col-sm-7">
           <h3 class="welcome_text">WELCOME</h3>
           <h2 class="tuffy_text">TUFFY's LAIR</h2>
-          <p class="tuffy_intro f-17">嗨，你好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的小玩意，祝您在此探索愉快,嗨，你好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的小玩意，祝您在此探索愉快,嗨，你好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的小玩意，祝您在此探索愉快</p>
+          <p class="tuffy_intro f-17">嗨，您好，我是tuffy，一名前端工作者，欢迎来到我的个人网站，这里有我的求职简历，个人博客，以及一些有趣的玩意，祝您在此探...，哦！还有，差点忘了，点击下方的星球（它们分别是地球，土星和太阳）可以进入我的个人网站不同的区域，请尽情探索这片“星系”吧，如果愿意的话，欢迎注册游玩！</p>
           <button class="enter_button f-17" :style="currentTopic.buttonColor" @click="toLink(currentTopic.link)">{{ currentTopic.buttonText }}</button>
         </div>
-        <div class="col-xl-4 col-lg-6" style="text-align: right;">
-          <img :src='currentTopic.src' />
+        <div class="col-xl-4 col-lg-4 col-sm-2" style="text-align: right;">
+          <img :src='currentTopic.src' class="big_img" />
         </div>
       </div>
       <div class="exchange_box">
@@ -36,31 +36,31 @@ export default {
   data () {
     return {
       currentTopic: {
-        bgColor: 'background: #1F1E1F',
-        buttonColor: 'color: #1F1E1F',
-        src: require('../assets/blog.svg'),
+        bgColor: 'background: #115920',
+        buttonColor: 'color: #115920',
+        src: require('../assets/地球.svg'),
         buttonText: '心情愉悦，来读篇博客吧',
         link: '/blog'
       },
       exs: [
         {
-          bgColor: 'background: #1F1E1F',
-          buttonColor: 'color: #1F1E1F',
-          src: require('../assets/blog.svg'),
+          bgColor: 'background: #115920',
+          buttonColor: 'color: #115920',
+          src: require('../assets/地球.svg'),
           buttonText: '心情愉悦，来读篇博客吧',
           link: '/blog'
         },
         {
-          bgColor: 'background: #0261BF',
-          buttonColor: 'color: #0261BF',
-          src: require('../assets/resume.svg'),
+          bgColor: 'background: #914C12',
+          buttonColor: 'color: #914C12',
+          src: require('../assets/土星.svg'),
           buttonText: '心情愉悦，来阅批简历吧',
           link: '/resume'
         },
         {
-          bgColor: 'background: indigo',
-          buttonColor: 'color: indigo',
-          src: require('../assets/ninja.svg'),
+          bgColor: 'background: #FD7C2E',
+          buttonColor: 'color: #FD7C2E',
+          src: require('../assets/太阳.svg'),
           buttonText: '心情愉悦，来玩个游戏吧',
           link: '/blog'
         }
@@ -115,7 +115,7 @@ button:focus {
 }
 .start_title_back {
   clip-path: circle(0% at 0% 0%);
-  animation: circle-in 1s 1s linear forwards;
+  animation: circle-in 1.7s .5s linear forwards;
 }
 @keyframes up-in {
   0% {
@@ -161,20 +161,25 @@ button:focus {
 .row-vertical-center {
   display: flex;
   align-items: center;
+  cursor: default;
 }
 .welcome_text {
   font-size: 150px;
   line-height: 100px;
   color: transparent;
-  -webkit-text-stroke: #4E474F 1px;
+  /* -webkit-text-stroke: #4E474F 1px; */
   animation: color-in 2s 3s ease forwards;
+  cursor: default;
+  transition: all ease .5s;
 }
 .tuffy_text {
   font-size: 90px;
   font-weight: bolder;
   color: transparent;
-  -webkit-text-stroke: #4E474F 1px;
+  /* -webkit-text-stroke: #4E474F 1px; */
   animation: color-in 2s 4s ease forwards;
+  cursor: default;
+  transition: all ease .5s;
 }
 @keyframes color-in {
   0% {
@@ -190,6 +195,7 @@ button:focus {
   width: 800px;
   text-align: justify;
   margin: 30px 0;
+  transition: all ease .5s;
 }
 .enter_button {
   padding: 10px 30px;
@@ -206,7 +212,9 @@ button:focus {
   bottom: 30px;
   margin: 0 auto;
   left: 50%;
+  width: 100%;
   transform: translateX(-50%);
+  text-align: center;
 }
 .exchange_li {
   display: inline-block;
@@ -221,12 +229,133 @@ button:focus {
   width: 90px;
   height: 90px;
 }
-@keyframes bowen {
-  0%, 100% {
-    clip-path: polygon(0% 50%, 9% 42%, 23% 34%, 33% 36%, 41% 44%, 50% 54%, 57% 63%, 68% 68%, 79% 67%, 88% 63%, 98% 54%, 100% 100%, 0% 100%);
+.big_img {
+  animation: upordown ease 5s infinite forwards;
+}
+@keyframes upordown {
+  0% {
+    transform: translateY(-100px);
   }
   50% {
-    clip-path: polygon(0% 50%, 11% 59%, 21% 67%, 32% 73%, 41% 70%, 47% 60%, 55% 50%, 65% 42%, 76% 34%, 90% 39%, 99% 54%, 100% 100%, 0% 100%);
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-100px);
+  }
+}
+@media screen and (max-width: 1900px) {
+  .welcome_text {
+    font-size: 120px;
+    line-height: 90px;
+  }
+  .tuffy_text {
+    font-size: 80px
+  }
+  .tuffy_intro {
+    width: 700px;
+  }
+  .big_img {
+    width: 800px;
+    height: 800px;
+  }
+}
+@media screen and (max-width: 1600px) {
+  .welcome_text {
+    font-size: 110px;
+    line-height: 80px;
+  }
+  .tuffy_text {
+    font-size: 60px
+  }
+  .tuffy_intro {
+    width: 600px;
+  }
+  .big_img {
+    width: 700px;
+    height: 700px;
+  }
+}
+@media screen and (max-width: 1400px) {
+  .welcome_text {
+    font-size: 90px;
+    line-height: 70px;
+  }
+  .tuffy_text {
+    font-size: 60px
+  }
+  .tuffy_intro {
+    width: 500px;
+  }
+  .big_img {
+    width: 600px;
+    height: 600px;
+  }
+}
+@media screen and (max-width: 1200px) {
+  .welcome_text {
+    font-size: 80px;
+    line-height: 60px;
+  }
+  .tuffy_text {
+    font-size: 50px
+  }
+  .tuffy_intro {
+    width: 450px;
+  }
+  .big_img {
+    width: 500px;
+    height: 500px;
+  }
+}
+@media screen and (max-width: 1030px) {
+  .welcome_text {
+    font-size: 70px;
+    line-height: 50px;
+  }
+  .tuffy_text {
+    font-size: 40px
+  }
+  .tuffy_intro {
+    width: 400px;
+    font-size: 10px;
+  }
+  .big_img {
+    width: 500px;
+    height: 500px;
+  }
+}
+@media screen and (max-width: 575px) {
+  .welcome_text {
+    font-size: 70px;
+  }
+  .tuffy_text {
+    font-size: 40px;
+  }
+  .tuffy_intro {
+    width: 100%;
+    font-size: 10px;
+  }
+  .big_img {
+    width: 500px;
+    height: 500px;
+    display: none;
+  }
+  .exchange_img {
+    width: 70px;
+    height: 70px;
+  }
+}
+@media screen and (max-width: 350px) {
+  .welcome_text {
+    font-size: 50px;
+    line-height: 30px;
+  }
+  .tuffy_text {
+    font-size: 30px;
+  }
+  .exchange_img {
+    width: 50px;
+    height: 50px;
   }
 }
 </style>
